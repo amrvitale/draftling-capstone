8974c1af705581755ae6d9c7d069424bb68f5875
Angela Bumgarner <amrvitale@gmail.com>
2020-07-10 19:42:40 -0700

    fixed getCritiquesForDraftlings in draftlings-helpers.js

--- src/draftlings-helpers.js
+++ src/draftlings-helpers.js
@@ -1,14 +1,8 @@
 export const findDraftling = (draftlings=[], draftling_id) =>
   draftlings.find(draftling => draftling.id === draftling_id)
 
-
-  
-   
-  export const getCritiquesForDraftlings = (draftling_id) => {
-    let templateCrits = this.context.templateCrits || [];
-    let freeformCrits = this.context.templateCrits || [];
-    
+  export const getCritiquesForDraftlings = (critiques, draftling_id) => {
     return (!draftling_id)
-    ? templateCrits.concat(freeformCrits)
-    : templateCrits.filter(templateCrit => templateCrit.draftling_id === draftling_id).concat(freeformCrits.filter(freeformCrit => freeformCrit.draftling_id === draftling_id));
+      ? critiques
+      : critiques.filter(critique => critique.draftling_id === draftling_id);
     }
